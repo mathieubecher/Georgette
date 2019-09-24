f72c1fb11fa3519ccb4275886529abe4b058bca4
Becher <mbelkhenchir@gmail.com>
2019-09-24 11:30:38 +0200

    redfine map constructor

--- Georgette/Map.cpp
+++ Georgette/Map.cpp
@@ -1,4 +1,7 @@
 #include "pch.h"
 #include "Map.h"
 
+Map::Map(std::string file, int x, int y, int width, int height) : Physic2D(file,x,y,width,height)
+{
 
+}
--- Georgette/Map.h
+++ Georgette/Map.h
@@ -3,5 +3,6 @@
 class Map: public Physic2D
 {
 public:
+	Map(std::string file, int x = 0, int y = 0, int width = 1, int height = 1);
 };
 
