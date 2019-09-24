5e68c84e97c7f9f04d9c76824493c28a59297243
Simon <simongigant39@gmail.com>
2019-09-24 12:46:43 +0200

    GetChunk

--- Georgette/MapGenerator.cpp
+++ Georgette/MapGenerator.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "MapGenerator.h"
+#include "Game.h"
 
 Vector2 const MapGenerator::SIZE(80,40);
 
@@ -13,10 +14,15 @@ MapGenerator::~MapGenerator()
 }
 
 Map *MapGenerator::GenerateChunk(Vector2 pos) {
+
 	return nullptr;
 }
 
 
 Map *MapGenerator::FindChunk(Vector2 pos) {
+	Game::GetChunks();
+
+
+
 	return nullptr;
 }
