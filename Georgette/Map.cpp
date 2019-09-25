da47f4d8c8602a7eeac68291ea1a94171e761f72
Simon <simongigant39@gmail.com>
2019-09-25 13:01:27 +0200

    merge correction

--- Georgette/Map.cpp
+++ Georgette/Map.cpp
@@ -9,23 +9,24 @@ Map::Map(std::string file, int x, int y, int width, int height) : Physic2D(file,
 	this->size = this->sprite.GetSize();
 }
 Box Map::Collider(Vector2f pos, Vector2 size) {
-}
-	this->size = this->sprite.GetSize();
-	g->AddChunk(this);
-	Game *g = Game::Get();
-Map::Map(CHAR_INFO *sprite, int x, int y, int width, int height) : Physic2D(sprite, Vector2(width, height), Vector2f(0, 0)) {
 	for (int x = ceil(pos.x); x < ceil(pos.x) + size.x; ++x) {
 		for (int y = ceil(pos.y); y < ceil(pos.y) + size.y; ++y) {
 
-			if(x >= this->pos.x && 
-				x < this->pos.x + this->size.x && 
-				y >= this->pos.y && 
-				y < this->pos.y + this->size.y){
+			if (x >= this->pos.x &&
+				x < this->pos.x + this->size.x &&
+				y >= this->pos.y &&
+				y < this->pos.y + this->size.y) {
 				if ((this->sprite.GetCase(x - this->pos.x, y - this->pos.y).Attributes & 0x00f0) == 0) {
-						return Box(x,y,1,1);
+					return Box(x, y, 1, 1);
 				}
 			}
 		}
 	}
 	return Box();
+}
+
+Map::Map(CHAR_INFO *sprite, int x, int y, int width, int height) : Physic2D(sprite, Vector2(width, height), Vector2f(0, 0)) {
+	Game *g = Game::Get();
+	g->AddChunk(this);
+	this->size = this->sprite.GetSize();
 }
