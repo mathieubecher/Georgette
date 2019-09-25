63a614867c3b971dbd008dbd09a2418d161ffd09
Simon <simongigant39@gmail.com>
2019-09-25 15:39:07 +0200

    Génération un peu meilleure

--- Georgette/MapGenerator.cpp
+++ Georgette/MapGenerator.cpp
@@ -22,7 +22,7 @@ CHAR_INFO *MapGenerator::GenerateDirtBlock(Vector2 &size) {
 	size_t randValue;
 	for (size_t i = 0; i < n; ++i) {
 		res[i].Char.UnicodeChar = ' ';
-		res[i].Attributes = 0x00f0;
+		res[i].Attributes = 0x00d0;
 		if (std::rand() % n != 0) {
 			randValue = std::rand() % 100;
 			res[i].Attributes = 0x000f;
@@ -70,23 +70,91 @@ void MapGenerator::PutSprite(CHAR_INFO *fullSprite, size_t x, size_t y) {
 	delete [] res;
 }
 
+int MapGenerator::GetRandomFloorLevel() {
+	int res = SIZEH - rand() % (SIZEH * 2 / 3);
+	return res - (res % 4);
+}
+
+GenFormat MapGenerator::GenerateFormat(Map *left, Map *right, Map *top, Map *bottom) {
+	GenFormat res;
+	if (right) {
+		res.right = right->format.left;
+	}
+	else if (rand()%10 != 0) {
+		res.right = MapGenerator::GetRandomFloorLevel();
+	}
+	else {
+		res.right = -1;
+	}
+	if (left) {
+		res.left = left->format.right;
+	}
+	else if (rand() % 10 != 0) {
+		res.left = MapGenerator::GetRandomFloorLevel();
+	}
+	else {
+		res.left = -1;
+	}
+	if (top) {
+		res.top = top->format.bottom;
+	}
+	else {
+		res.top = rand() % 2 == 0;
+	}
+	if (bottom) {
+		res.bottom = bottom->format.top;
+	}
+	else {
+		res.bottom = rand() % 2 == 0;
+	}
+	return res;
+}
+
 Map *MapGenerator::GenerateFirstChunk() {
 	srand(time(NULL));
+	/*
 	CHAR_INFO *sprite = new CHAR_INFO[SIZEW*SIZEH];
 	for (size_t j = 0; j < SIZEW*SIZEH; ++j) {
-		sprite[j].Attributes = 0x00f0;
+		sprite[j].Attributes = 0x00d0;
 		sprite[j].Char.UnicodeChar = ' ';
 	}
 	for (size_t i = 0; i < 10; ++i) {
 		PutSprite(sprite,i*8,36);
 	}
 	Map *first = new Map(sprite, 0, 0);
+	first->format = MapGenerator::GenerateFormat(nullptr,nullptr,nullptr,nullptr);
 	return first;
+	*/
+	return GenerateChunk(Vector2(0,0));
 }
 
 Map *MapGenerator::GenerateChunk(Vector2 pos) {
-
-	return nullptr;
+	CHAR_INFO *sprite = new CHAR_INFO[SIZEW*SIZEH];
+	for (size_t j = 0; j < SIZEW*SIZEH; ++j) {
+		sprite[j].Attributes = 0x00d0;
+		sprite[j].Char.UnicodeChar = ' ';
+	}
+	GenFormat format = MapGenerator::GenerateFormat(FindChunk(Vector2(pos.x-SIZEW,pos.y)), FindChunk(Vector2(pos.x + SIZEW, pos.y)), FindChunk(Vector2(pos.x, pos.y-SIZEH)), FindChunk(Vector2(pos.x, pos.y + SIZEH)));
+	int floorLevel = format.left == -1 ? MapGenerator::GetRandomFloorLevel() : format.left;
+	int targetLevel = format.right == -1 ? MapGenerator::GetRandomFloorLevel() : format.right;
+	int sign = 0;
+	if (targetLevel != floorLevel) {
+		sign = (targetLevel - floorLevel) / abs(targetLevel - floorLevel);
+	}
+	for (size_t i = 0; i < 10; ++i) {
+		if (floorLevel != targetLevel) {
+			int randValue = rand() % 3;
+			if (randValue == 0) {
+				floorLevel += sign * 4;
+			}
+		}
+		for (size_t j = SIZEH - floorLevel; j < SIZEH; j+=4) {
+			PutSprite(sprite, i * 8, j);
+		}
+	}
+	Map *res = new Map(sprite, 0, 0);
+	res->format = format;
+	return res;
 }
 
 
